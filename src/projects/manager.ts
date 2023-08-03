dbd36ff59b668b7f5a8defcffcc3da5ab2718856
yaegassy <yaegassy@users.noreply.github.com>
2023-08-03 23:29:31 +0900

    refactor: remove onDidOpenTextDocument event etc, of project manager

--- src/projects/manager.ts
+++ src/projects/manager.ts
@@ -1,4 +1,4 @@
-import { ExtensionContext, OutputChannel, workspace } from 'coc.nvim';
+import { OutputChannel, workspace } from 'coc.nvim';
 
 import { SUPPORTED_LANGUAGE } from '../constant';
 import { BladeProjectsManager } from './managers/blade';
@@ -8,7 +8,7 @@ import { TranslationProjectManager } from './managers/translation';
 import { ViewReferenceProjectManager } from './managers/viewReference';
 import { type ProjectManagerType } from './types';
 
-export async function register(context: ExtensionContext, outputChannel: OutputChannel) {
+export async function register(outputChannel: OutputChannel) {
   const { document } = await workspace.getCurrentState();
 
   if (!SUPPORTED_LANGUAGE.includes(document.languageId)) return;
@@ -27,53 +27,6 @@ export async function register(context: ExtensionContext, outputChannel: OutputC
 
   const livewireProjectManager = new LivewireProjectManager(workspace.root, outputChannel);
   await livewireProjectManager.initialize();
-
-  // FIRST OPEN
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  workspace.documents.map(async (doc) => {
-    if (SUPPORTED_LANGUAGE.includes(doc.languageId)) {
-      if (!bladeProjectManager.isInitialized()) {
-        await bladeProjectManager.initialize();
-      }
-      if (!viewReferenceProjectManager.isInitialized()) {
-        await viewReferenceProjectManager.initialize();
-      }
-      if (!translationProjectManager.isInitialized()) {
-        await translationProjectManager.initialize();
-      }
-      if (!phpFunctionProjectManager.isInitialized()) {
-        await phpFunctionProjectManager.initialize();
-      }
-      if (!livewireProjectManager.isInitialized()) {
-        await livewireProjectManager.initialize();
-      }
-    }
-  });
-
-  // DID OPEN
-  workspace.onDidOpenTextDocument(
-    async (e) => {
-      if (SUPPORTED_LANGUAGE.includes(e.languageId)) {
-        if (!bladeProjectManager.isInitialized()) {
-          await bladeProjectManager.initialize();
-        }
-        if (!viewReferenceProjectManager.isInitialized()) {
-          await viewReferenceProjectManager.initialize();
-        }
-        if (!translationProjectManager.isInitialized()) {
-          await translationProjectManager.initialize();
-        }
-        if (!phpFunctionProjectManager.isInitialized()) {
-          await phpFunctionProjectManager.initialize();
-        }
-        if (!livewireProjectManager.isInitialized()) {
-          await livewireProjectManager.initialize();
-        }
-      }
-    },
-    null,
-    context.subscriptions
-  );
 
   const projectManagers: ProjectManagerType = {
     bladeProjectManager,
