66eecc79adc57ff2bdd47b30489a16261c4f817e
yaegassy <yaegassy@users.noreply.github.com>
2023-09-12 18:37:46 +0900

    refactor(definition): add direct search by bladeKey in livewireTag definition

--- src/definitions/handlers/livewireTagHandler.ts
+++ src/definitions/handlers/livewireTagHandler.ts
@@ -22,25 +22,42 @@ export async function doDefinition(
 
   if (!text.startsWith('livewire:')) return [];
 
+  //
+  // Search by bladeProjectManager with bladeKey
+  //
+
+  const bladeKey = text.replace('livewire:', 'livewire.');
+  const blade = bladeProjectManager.bladeMapStore.get(bladeKey);
+  if (blade) {
+    const bladeLocation: Location = {
+      uri: Uri.parse(blade).toString(),
+      range: Range.create(Position.create(0, 0), Position.create(0, 0)),
+    };
+    locations.push(bladeLocation);
+  }
+
+  //
+  // Search by livewireProjectManager with livewireKey
+  //
+
   const livewireKey = text.replace('livewire:', '');
-
   const livewire = livewireProjectManager.livewireMapStore.get(livewireKey);
-  if (!livewire) return [];
+  if (livewire) {
+    const livewireLocation: Location = {
+      uri: Uri.parse(livewire.filePath).toString(),
+      range: Range.create(Position.create(0, 0), Position.create(0, 0)),
+    };
+    locations.push(livewireLocation);
 
-  const location: Location = {
-    uri: Uri.parse(livewire.filePath).toString(),
-    range: Range.create(Position.create(0, 0), Position.create(0, 0)),
-  };
-  locations.push(location);
-
-  if (livewire.templateKey) {
-    const bladeFilePath = bladeProjectManager.bladeMapStore.get(livewire.templateKey);
-    if (bladeFilePath) {
-      const location: Location = {
-        uri: Uri.parse(bladeFilePath).toString(),
-        range: Range.create(Position.create(0, 0), Position.create(0, 0)),
-      };
-      locations.push(location);
+    if (livewire.templateKey) {
+      const bladeFilePath = bladeProjectManager.bladeMapStore.get(livewire.templateKey);
+      if (bladeFilePath) {
+        const location: Location = {
+          uri: Uri.parse(bladeFilePath).toString(),
+          range: Range.create(Position.create(0, 0), Position.create(0, 0)),
+        };
+        locations.push(location);
+      }
     }
   }
 
